e7716af858322803f2a30a7fde0e4acb24e6b831
ass106 <43350367+ass106@users.noreply.github.com>
2018-10-03 12:03:13 +0000

    Initial commit

---
+++ daemi3.java
@@ -0,0 +1,33 @@
+/*************************************************************
+ *                      Tölvunarfræði 1                      *
+ *                       Heimadæmi 1.3                       *
+ * Forrit þetta prentar út þennan fallega ramma fyrir neðan. *
+ *                                                           *
+ *         /  \  /  \  /  \  /  \  /  \  /  \  /  \          *
+ *         \  /  \  /  \  /  \  /  \  /  \  /  \  /          *
+ *         /  \                                /  \          *
+ *         \  /                                \  /          *
+ *         /  \         Tölvunarfræði 1        /  \          *
+ *         \  /           Haust 2018           \  /          *
+ *         /  \                                /  \          *
+ *         \  /                                \  /          *
+ *         /  \  /  \  /  \  /  \  /  \  /  \  /  \          *
+ *         \  /  \  /  \  /  \  /  \  /  \  /  \  /          *
+ *                                                           *
+ *************************************************************/
+
+public class daemi3 {
+
+    public static void main(String[] args) {
+        System.out.println("/  \\  /  \\  /  \\  /  \\  /  \\  /  \\  /  \\");
+        System.out.println("\\  /  \\  /  \\  /  \\  /  \\  /  \\  /  \\  /");
+        System.out.println("/  \\                                /  \\");
+        System.out.println("\\  /                                \\  /");    
+        System.out.println("/  \\         Tölvunarfræði 1        /  \\");
+        System.out.println("\\  /           Haust 2018           \\  /");    
+        System.out.println("/  \\                                /  \\");
+        System.out.println("\\  /                                \\  /");        
+        System.out.println("/  \\  /  \\  /  \\  /  \\  /  \\  /  \\  /  \\");
+        System.out.println("\\  /  \\  /  \\  /  \\  /  \\  /  \\  /  \\  /");
+    }
+}
---
+++ segdir.java
@@ -0,0 +1,14 @@
+public class segdir {
+
+    public static void main(String []args) {
+
+        int a = 2 / (3 / 2);
+        int b = 2 / 3 / 2;
+        int c = (2 + 2);
+        int d = 2 + 2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("2" + c);
+        System.out.println("2" + 2 * 2);
+    }
+}
